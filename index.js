8a8293b34b72504452d236021550867475917ec7
Iddar Olivares <iddar@customer-GDL-194-67.megared.net.mx>
2015-10-11 13:18:39 -0500

    FIX

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ var WEBPORT = 3000
 var devices = require('./config/devices')
 var PeriodicActivity = require('./lib/periodic-activity')
 
-var btUpdater = {
-  a: new PeriodicActivity(devices['intput_1'], function (newValue) {
-    io.emit('controlUpdate', {intput_1: !!newValue})
-  }),
-  b: new PeriodicActivity(devices['intput_2'], function (newValue) {
-    io.emit('controlUpdate', {intput_2: !!newValue})
-  }),
-  c: new PeriodicActivity(devices['intput_3'], function (newValue) {
-    io.emit('controlUpdate', {intput_3: !!newValue})
-  })
-}
+var intput_1 = new PeriodicActivity(devices['intput_1'], function (newValue) {
+  io.emit('controlUpdate', {'intput_1': !!newValue})
+})
+
+var intput_2 = new PeriodicActivity(devices['intput_2'], function (newValue) {
+  io.emit('controlUpdate', {'intput_2': !!newValue})
+})
+
+var intput_3 = new PeriodicActivity(devices['intput_3'], function (newValue) {
+  io.emit('controlUpdate', {'intput_3': !!newValue})
+})
 
 function getRootCallback (req, res) {
   res.sendfile('index.html')

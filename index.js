2260df0b11cb97032ef318eb0bdb954998628b14
Iddar Olivares <iddar@customer-GDL-194-67.megared.net.mx>
2015-10-11 13:48:15 -0500

    add temperature sensor

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var _ = require('lodash')
 var WEBPORT = 3000
 var devices = require('./config/devices')
 var PeriodicActivity = require('./lib/periodic-activity')
+var AnalogUpdater = require('./lib/analog-updater')
+var setTemperature = require('./lib/set-temperature')
+
+var tempSensor = new AnalogUpdater(0, function (newValue) {
+  io.emit('controlUpdate', {'temperature': newValue})
+  setTemperature('temperatura', newValue)
+})
 
 var intput_1 = new PeriodicActivity(devices['intput_1'], function (newValue) {
   io.emit('controlUpdate', {'intput_1': !!newValue})

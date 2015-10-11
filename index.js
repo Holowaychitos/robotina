7bd3d8c053f1b3b2a4b7e7572c272b220f2b367f
iddar <iddar@dbug.mx>
2015-10-11 07:30:09 +0000

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+var WEBPORT = 3000 // Puerto en el que escucha el servidor web
+
+var mraa = require('mraa')
+console.log('MRAA Version: ' + mraa.getVersion())
+var pin13 = new mraa.Gpio(13)
+pin13.dir(mraa.DIR_OUT)
+
+
+var objControl = {'led': pin13}
+
+function getRootCallback (req, res) {
+  res.sendfile('index.html')
+}
+
+// GET /: Crea la ruta '/' y lanza un callback
+app.get('/', getRootCallback)
+
+io.on('connection', function onConnection (socket) {
+  console.log('Dentro')
+  socket.on('controlHandler', function (data) {
+    console.log('Data:', data)
+    objControl[data.control].write(parseState(data.value))
+    io.emit('controlUpdate', data)
+  })
+})
+
+function parseState(value) {
+  return value ? 1 : 0
+}
+
+// Crea el servidor y define el puerto en el que escucha
+http.listen(WEBPORT, function serverStart () {
+  console.log(WEBPORT)
+})
+

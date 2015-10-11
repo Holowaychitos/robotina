d5c047873ac09bb3484a5367c917ec188ee9d678
Iddar Olivares <iddar@customer-GDL-194-67.megared.net.mx>
2015-10-11 14:01:47 -0500

    add temperature fix

--- config/devices.js
+++ config/devices.js
@@ -2,6 +2,8 @@ var ioFactory = require('../lib/io-factory')
 
 module.exports = {
   'output_1': ioFactory(13, 'OUT'),
+  'output_2': ioFactory(12, 'OUT'),
+  'output_3': ioFactory(11, 'OUT'),
   'intput_1': ioFactory(5, 'IN'),
   'intput_2': ioFactory(6, 'IN'),
   'intput_3': ioFactory(7, 'IN')
